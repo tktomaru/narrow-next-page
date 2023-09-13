5e8c739c20405bfafebdaebea8972137cd8552c3
toma <58778944+tktomaru@users.noreply.github.com>
2023-09-13 23:20:16 +0900

    なろうに対応

--- content.js
+++ content.js
@@ -1,32 +1,68 @@
-document.addEventListener('keydown', function(event) {
-    console.log('Down arrow  key=' + event.key  + "  code=" + event.code)
+document.addEventListener('keydown', function (event) {
+    console.log('Down arrow  key=' + event.key + "  code=" + event.code)
+    // ============= ハーメルン用 ============= 
     if (event.key === 'PageDown') {
-        console.log('Down arrow pressed')
+        console.log('HAMELN Down arrow pressed')
         // ページの最下部までスクロールされたかをチェック
         if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-            console.log('Down arrow pressed and scrolled to bottom')
+            console.log('HAMELN Down arrow pressed and scrolled to bottom')
             // class属性がnext_page_linkである要素を探す
             var nextHref = document.querySelector('.next_page_link');
             if (nextHref) {
-                console.log('Down arrow pressed and scrolled to bottom and next page link found')
+                console.log('HAMELN Down arrow pressed and scrolled to bottom and next page link found')
                 // hrefをクリック
                 nextHref.click();
             }
         }
     }
     if (event.key === 'PageUp') {
-        console.log('Up arrow pressed')
+        console.log('HAMELN Up arrow pressed')
         // ページの最上部までスクロールされたかをチェック
         if ((window.scrollY) <= 0) {
-            console.log('Up arrow pressed and scrolled to top')
+            console.log('HAMELN Up arrow pressed and scrolled to top')
             // class属性がnovelnbである要素を探す
             var nextLi = document.querySelector('.novelnb');
             if (nextLi) {
                 var nextHref = nextLi.querySelector('a');
                 if (nextHref) {
-                    console.log('Up arrow pressed and scrolled to top and prev page link found')
+                    console.log('HAMELN Up arrow pressed and scrolled to top and prev page link found')
                     // hrefをクリック
                     nextHref.click();
+                }
+            }
+        }
+    }
+    
+    // ============= なろう用 ============= 
+    if (event.key === 'PageDown') {
+        console.log('Narrow Down arrow pressed')
+        // ページの最下部までスクロールされたかをチェック
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            console.log('Narrow Down arrow pressed and scrolled to bottom')
+            // class属性がnovel_bnである要素を探す
+            var nextDiv = document.querySelector('.novel_bn');
+            if (nextDiv) {
+                var nextHref = nextDiv.querySelectorAll('a');
+                if (nextHref) {
+                    console.log('Narrow Down arrow pressed and scrolled to top and next page link found')
+                    // hrefをクリック
+                    nextHref.item(1).click();
+                }
+            }
+        }
+    }
+    if (event.key === 'PageUp') {
+        console.log('Narrow Up arrow pressed')
+        // ページの最上部までスクロールされたかをチェック
+        if ((window.scrollY) <= 0) {
+            // class属性がnovel_bnである要素を探す
+            var nextDiv = document.querySelector('.novel_bn');
+            if (nextDiv) {
+                var nextHref = nextDiv.querySelectorAll('a');
+                if (nextHref) {
+                    console.log('Narrow Up arrow pressed and scrolled to Bottom and prev page link found')
+                    // hrefをクリック
+                    nextHref.item(0).click();
                 }
             }
         }
