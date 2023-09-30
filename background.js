8ca84a52a47ecf6305de841a4d050dc79ea0345e
tktomaru <tk.tomaru@gmail.com>
2023-09-30 22:19:49 +0900

    違う方法でもドメインを取得できた

--- background.js
+++ background.js
@@ -4,12 +4,12 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log('Extension installed')
 })
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.action === 'getCurrentURL') {
-    var currentURL = message.url
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      var tabId = tabs[0].id
-      chrome.tabs.sendMessage(tabId, { action: 'updateURL', url: currentURL })
-    })
-  }
-})
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//   if (message.action === 'getCurrentURL') {
+//     var currentURL = message.url
+//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//       var tabId = tabs[0].id
+//       chrome.tabs.sendMessage(tabId, { action: 'updateURL', url: currentURL })
+//     })
+//   }
+// })
--- content.js
+++ content.js
@@ -72,6 +72,9 @@ document.addEventListener('keydown', function (event) {
     func(parameter, clickFunc, text, selectClass)
   }
 
+  // console.log(window.location.href)
+  currentDomain = new URL(window.location.href).host
+  console.log('currentDomain:', currentDomain)
   // ============= ハーメルン用 =============
   if (currentDomain == 'syosetu.org') {
     if (event.key === 'PageDown') {
@@ -131,18 +134,18 @@ document.addEventListener('keydown', function (event) {
   }
 })
 
-// 現在のタブのURLを取得
-var currentURL = window.location.href
+// // 現在のタブのURLを取得
+// var currentURL = window.location.href
 
-// メッセージを送信
-chrome.runtime.sendMessage({ action: 'getCurrentURL', url: currentURL })
+// // メッセージを送信
+// chrome.runtime.sendMessage({ action: 'getCurrentURL', url: currentURL })
 
-var currentDomain = ''
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  var receivedURL = message.url
-  console.log('Received URL:', receivedURL)
-  if (receivedURL !== '') {
-    currentDomain = new URL(receivedURL).host
-    console.log('currentDomain:', currentDomain)
-  }
-})
+// var currentDomain = ''
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//   var receivedURL = message.url
+//   console.log('Received URL:', receivedURL)
+//   if (receivedURL !== '') {
+//     currentDomain = new URL(receivedURL).host
+//     console.log('currentDomain:', currentDomain)
+//   }
+// })
